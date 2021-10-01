48842a585f35b0ee0dc7abf1f58434b2df78a753
Sam Wooldridge <5wools45@solent.ac.uk>
2021-10-01 12:47:57 +0100

    Started string_operators task

---
+++ basics/input/string_operators.py
@@ -0,0 +1,9 @@
+# Improving beeps health program
+print("Please enter the number of lives")
+lives = int(input())
+print("Please enter the energy level")
+energy = int(input())
+print("Please enter the shield level")
+shield = int(input())
+print("Health has been set")
+

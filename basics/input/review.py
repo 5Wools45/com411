cf2ed97cfab0fb3a9066714942dfd58d19a6c540
SamWooldridge <5wools45@solent.ac.uk>
2021-10-03 19:48:11 +0100

    Finished review.py challenge

---
+++ basics/input/review.py
@@ -0,0 +1,9 @@
+#acknowledgement
+print("Hi There! My name is \'Beep\', whats yours?")
+#setting variables
+name = str(input())
+print(f"Hi {name}")
+print("How many houses do you have?")
+houses = int(input())
+print(f"That's this many houses! {'❤' * houses}")
+

2987889f272046669b80f6b2834825ab09355717
Sam Wooldridge <5wools45@solent.ac.uk>
2021-10-08 12:31:50 +0100

    Added code to demonstrate the use of a logical and operator

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,8 @@
+print("What did I hear?")
+noise = str(input())
+print("What did I see?")
+sight = str(input())
+if (noise == "grrr") and (sight == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue")

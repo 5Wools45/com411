5998822708f85b2e8eacbdf95daa9eec458b6039
SamWooldridge <5wools45@solent.ac.uk>
2021-10-10 18:01:18 +0100

    Added code to demonstrate the use of nested loops and decisions

--- basics/repetitions/nested_loop/nesting.py
+++ basics/repetitions/nested_loop/nesting.py
@@ -4,3 +4,16 @@ sequence = str(input())
 print("Please enter a character for the marker")
 marker = str(input())
 
+marker1_position = -1
+marker2_position = -1
+
+for position in range(0, len(sequence), 1):
+    letter = sequence[position]
+
+    if letter == marker:
+        if(marker1_position == -1):
+            marker1_position = position
+        else:
+            marker2_position = position
+
+print(f"The distance between the markers is {marker2_position - marker1_position -1}.")

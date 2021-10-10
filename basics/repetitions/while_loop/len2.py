f2f5c9cd2817b31487304da5c3171695ac7d0d75
SamWooldridge <5wools45@solent.ac.uk>
2021-10-10 12:14:35 +0100

    Added code to demonstrate the use of a while loop with len function

---
+++ basics/repetitions/while_loop/len2.py
@@ -0,0 +1,8 @@
+print("Please enter a phrase:")
+phrase = str(input())
+#start a bop count to track
+bops = 0
+#display length of phrase in "bops"
+while bops < len(phrase):
+    print("Bop ", end="")
+    bops +=1

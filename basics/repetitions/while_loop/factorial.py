93da8b31cf1a0c47524630adc18e966f0058efdd
SamWooldridge <5wools45@solent.ac.uk>
2021-10-10 12:40:45 +0100

    Added code to demonstrate the use of a while loop in calculating the factorial of a number

---
+++ basics/repetitions/while_loop/factorial.py
@@ -0,0 +1,11 @@
+print("Please enter a number?")
+number = int(input())
+#count must start multiplying by 1
+total = 1
+#count increases once every iteration for the factorial
+count = 0
+while count < number:
+    count +=1
+    total = total * count
+print(f"The factorial is {total}.")
+

9120b677bfd613f854daf616889d2790bf2efaf7
SamWooldridge <5wools45@solent.ac.uk>
2021-10-10 12:11:22 +0100

    Added code to demonstrate the use of len function but without a while loop because why

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,7 @@
+print("Please enter a phrase:")
+#ask the user for a str input
+phrase = str(input())
+#count how many objects in the phrase
+phrase_length = len(phrase)
+#output "Bop" for length of phrase
+print(f"{phrase_length * 'Bop '}")

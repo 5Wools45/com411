18b7c65b0a79933c6132df7911ae6cfcb9265e28
SamWooldridge <5wools45@solent.ac.uk>
2021-10-10 12:05:56 +0100

    Added code to demonstrate the use of a while loop with ascii art

---
+++ basics/repetitions/while_loop/ascii.py
@@ -0,0 +1,10 @@
+print("How many bars should be charged?")
+#getting users response:
+bars_to_charge = int(input())
+#adding a counter to track charged bars
+charged_bars = 0
+#adding while loop to track how many bars have been charged
+while charged_bars <= bars_to_charge:
+    print(f"Charging: {charged_bars * ' █'}")
+    charged_bars +=1
+

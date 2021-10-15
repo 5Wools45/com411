9314105c4d1b6777f2a3e769f19e5e9d26c44acc
Sam Wooldridge <5wools45@solent.ac.uk>
2021-10-15 11:12:33 +0100

    Added code to demonstrate the use of built in functions: len, ord

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,9 @@
+print("Program Started")
+print("Please enter a standard character")
+character = input()
+
+if len(character) == 1:
+    print(f"The ascii code for {character} is {ord(character)}")
+else:
+    print("Please only enter one character")
+print("Program ended")

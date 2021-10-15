03626096eeb34c9b59434de06187ca5bf52b2931
Sam Wooldridge <5wools45@solent.ac.uk>
2021-10-15 11:28:40 +0100

    Added code to demonstrate the use of built in function chr

---
+++ basics/functions/ascii_character.py
@@ -0,0 +1,11 @@
+print("Program Started")
+print("Please enter an Ascii code")
+ascii_code = int(input())
+
+if ascii_code > 31 and ascii_code < 127:
+    print(f"The character represented by the ascii code {ascii_code} is {chr(ascii_code)}.")
+else:
+    "Please enter a value within the printable ascii character boundaries (32-126)"
+
+print("Program Ended")
+

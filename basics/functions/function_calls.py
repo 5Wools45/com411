062c4b86abad19c9c45594ed6f02e92c6e49e421
Sam Wooldridge <5wools45@solent.ac.uk>
2021-10-15 12:50:17 +0100

    need to complete function_calls.py

---
+++ basics/functions/function_calls.py
@@ -0,0 +1,37 @@
+def display_box(word):
+    print("*******")
+    print(f"| {word} |")
+    print(f"******")
+
+
+def display_lower(word):
+
+
+def display_upper(word):
+
+
+def display_mirrored(word):
+
+
+
+def repeat(word):
+
+
+def run():
+    if(method == 1):
+        displaybox()
+    if(method == 2):
+        display_lower()
+    if(method == 3):
+        display_upper()
+    if(method) == 4:
+        display_mirrored()
+    if(method == 5):
+
+
+
+print("Please enter a word:")
+word = input()
+print("How would you like your word displayed?")
+print("1 = Box, 2 = lower, 3 = upper, 4 = mirrored, 5 = repeat")
+method = int(input())

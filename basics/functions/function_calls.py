ab2d9ec2d3d601f97b073d43667059521667e1ea
Sam Wooldridge <Sam@newforestfruit.com>
2021-10-21 13:42:38 +0100

    Added code to demonstrate the use of multiple function calls

--- basics/functions/function_calls.py
+++ basics/functions/function_calls.py
@@ -1,37 +1,62 @@
 def display_box(word):
-    print("*******")
+    num_dashes = 4 + len(word)
+    print("-" * num_dashes)
     print(f"| {word} |")
-    print(f"******")
+    print("-" * num_dashes)
 
 
 def display_lower(word):
-
+    print(word.lower())
 
 def display_upper(word):
-
+    print(word.upper())
 
 def display_mirrored(word):
-
+    mirrored = ""
+    for letter in reversed(word):
+        mirrored += letter
+    print(f"{word} | {mirrored}")
 
 
 def repeat(word):
+    print("How many times should the word be repeated?")
+    repetitions = int(input())
+
+    for repetition in range(repetitions):
+        if (repetition % 2 == 0):
+            print(display_lower(word))
+        else:
+            print(display_upper(word))
+
 
 
 def run():
+    print("Please enter a word:")
+
+    word = input()
+
+    print("What would you like to do with this word?")
+    print("[1] Display the word in a box")
+    print("[2] Display the word in lower case")
+    print("[3] Display the word in upper case")
+    print("[4] Display the word mirrored")
+    print("[5] Display the word repeated")
+    print("[6] Quit")
+
+    method = int(input())
+
     if(method == 1):
-        displaybox()
-    if(method == 2):
-        display_lower()
-    if(method == 3):
-        display_upper()
-    if(method) == 4:
-        display_mirrored()
-    if(method == 5):
+        display_box(word)
+    elif(method == 2):
+        display_lower(word)
+    elif(method == 3):
+        display_upper(word)
+    elif(method) == 4:
+        display_mirrored(word)
+    elif(method == 5):
+        repeat(word)
+    else:
+        print("Unknown Option")
 
+run()
 
-
-print("Please enter a word:")
-word = input()
-print("How would you like your word displayed?")
-print("1 = Box, 2 = lower, 3 = upper, 4 = mirrored, 5 = repeat")
-method = int(input())

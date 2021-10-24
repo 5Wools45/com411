d5bcacb53950ea37f578dcf23f137ab495d72b04
SamWooldridge <5wools45@solent.ac.uk>
2021-10-24 15:03:52 +0100

    Added code to extract data from the CSV file

---
+++ data/files/csv/extract_csv.py
@@ -0,0 +1,18 @@
+import csv
+
+def extract(path):
+    print("Extracting...")
+    names = ""
+    with open(path) as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for lines in csv_reader:
+            names += f"{lines[1]}\n"
+        print("Done! The extracted names are as follows:")
+        print(f"\n{names}")
+
+def run():
+    extract("bots.csv")
+
+if __name__ == "__main__":
+    run()

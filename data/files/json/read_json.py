433774f8b1dd8758907943c8982a0a6af40a6f7a
SamWooldridge <5wools45@solent.ac.uk>
2021-10-26 20:11:51 +0100

    working on read_json.py

---
+++ data/files/json/read_json.py
@@ -0,0 +1,17 @@
+import json
+def read(path):
+    with open(path) as file:
+        data = json.load(file)
+        city = data["city"]
+        population = data["population"]
+        print(f"City Name: {city} \nPopulation: {population}")
+        for bots in data["bots"]:
+            name = bots["name"]
+            stats = bots["stats"]
+            print(f"{name} has the follow {stats}:")
+
+def run():
+    read("robocity.json")
+
+if __name__ == "__main__":
+    run()

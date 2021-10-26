779dccbacddeb31f8041f0987c3fb4bcce462537
SamWooldridge <5wools45@solent.ac.uk>
2021-10-26 20:14:18 +0100

    Added code to read a JSON file

--- data/files/json/read_json.py
+++ data/files/json/read_json.py
@@ -1,4 +1,6 @@
 import json
+
+
 def read(path):
     with open(path) as file:
         data = json.load(file)
@@ -10,8 +12,10 @@ def read(path):
             stats = bots["stats"]
             print(f"{name} has the follow {stats}:")
 
+
 def run():
     read("robocity.json")
+
 
 if __name__ == "__main__":
     run()

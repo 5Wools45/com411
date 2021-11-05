1589c37a8d19ff56f9e49b6dd060036846950bba
Sam Wooldridge <5wools45@solent.ac.uk>
2021-11-05 11:36:30 +0000

    simple set

---
+++ data/sets/simple_set.py
@@ -0,0 +1,8 @@
+def observed():
+    observations = set()
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+
+def run():
+    print(observed())

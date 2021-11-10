42854e7b085400acc1a076573d1c451f864b6b7d
SamWooldridge <5wools45@solent.ac.uk>
2021-11-10 19:17:00 +0000

    Added code to to demonstrate how to create a dictionary of dictionaries

---
+++ data/dicts/nested_dicts.py
@@ -0,0 +1,27 @@
+def short_pattern():
+    pattern = {"sequence": "101", "occurrences": "5"}
+    return pattern
+
+
+def medium_pattern():
+    pattern = {"sequence": "111000", "occurrences": "25"}
+    return pattern
+
+
+def long_pattern():
+    pattern = {"sequence": "1100110011001100", "occurrences": "50"}
+    return pattern
+
+
+def run():
+    print("Analysing patterns...")
+    new_dict = {
+        "short sequence": short_pattern(),
+        "medium sequence": medium_pattern(),
+        "long sequqnce": long_pattern()
+    }
+    for key, value in new_dict.items():
+        print(f"{key}, {value}")
+
+
+run()

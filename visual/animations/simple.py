6fb1d19a7393de6cc52ecb828a9f6c5c196df50f
Sam Wooldridge <5wools45@solent.ac.uk>
2021-11-26 11:39:24 +0000

    added code to demonstrate a simple animation and an animated marker

---
+++ visual/animations/simple.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+
+def animate(frame):
+    global ax
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
+    ax.plot(frame, frame, 'ro')
+
+
+def run():
+    simple_animation = animation.FuncAnimation(fig, animate, frames=10, interval=1000)
+    plt.show()
+
+
+run()
